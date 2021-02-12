11b51e18194072ebd249c65531bd90875231104d
Kelynn23 <kelynn.c19@gmail.com>
2021-02-12 14:54:58 -0800

    Finished Part 1 and 2, 3 ongoing

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,85 @@
 // product-item.js
-
+//constructor 
 class ProductItem extends HTMLElement {
-  // TODO
-}
+  constructor() {
+    //alert("reached constructor");
+    super(); 
+      // Attach a shadow root
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    let style = document.createElement('style');
+    let li = document.createElement('li');
+
+    li.setAttribute("class", "product");
+    let image = document.createElement('img');
+    let titlePara = document.createElement('p');
+    titlePara.setAttribute("class", "title");
+    let pricePara = document.createElement('p');
+    pricePara.setAttribute("class", "price");
+    let button = document.createElement('button');
+    button.setAttribute("onclick", "alert('Added to Cart!')");
+
+    // attach the created elements to the shadow dom
+    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(li);
+    li.appendChild(image);
+    li.appendChild(titlePara);
+    li.appendChild(pricePara);
+    li.appendChild(button);
+    
+    style.textContent = `
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }
+    `;
+  }
+
+  set imgSrc(src){
+    this.shadowRoot.querySelector("img").src=src;
+  };
+};
+
+
+function connectedCallback() {
+  console.log('Custom square element added to page.');
+  updateStyle(this);
+};
 
 customElements.define('product-item', ProductItem);
+
+//for button: define function inside script.js, set that function that modifies cart +memory stuff
+// to be event listener
--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,28 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if(localStorage.getItem("allProducts") == null){
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => localStorage.setItem("allProducts", JSON.stringify(data)))
+  }
+
+  let theProducts = JSON.parse(localStorage.getItem("allProducts"));
+  console.log(theProducts);
+  /*
+  let itemList = document.getElementById('product-list');
+  for(){//loop through the array
+    let prod = document.createElement('product-item');
+    itemList.appendChild(prod);
+  }
+  */
+  let itemList = document.getElementById('product-list');
+  for (product of theProducts){
+      let curProd = document.createElement('product-item');
+      //call a function that sets all attributes
+      //curProd.setProdAtt(product.id, product.title, product.price, 
+                        //product.description, product.image, product.category)
+      itemList.appendChild(curProd);
+  }
+
 });
